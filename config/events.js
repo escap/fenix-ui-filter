4dceb78af3ef4cdcf1be13ad9ac9432d875612e2
danielefenix <daniele.salvatore@fao.org>
2015-06-23 15:36:21 +0200

    edit event prefix

--- config/events.js
+++ config/events.js
@@ -6,23 +6,11 @@ define(function () {
 
     return {
         //Filter
-        MODULE_SELECT: "fx.catalog.module.select",
-        MODULE_REMOVE: "fx.catalog.module.remove",
-        MODULE_READY : "fx.catalog.module.ready",
-        MODULE_DESELECT : "fx.catalog.module.deselect",
+        MODULE_SELECT: "fx.filter.module.select",
+        MODULE_REMOVE: "fx.filter.module.remove",
+        MODULE_READY : "fx.filter.module.ready",
+        MODULE_DESELECT : "fx.filter.module.deselect",
 
-
-        //Main Controller
-
-        SEARCH_SUBMIT : "fx.catalog.submit",
-        SEARCH_QUERY_END : "fx.catalog.query.end",
-        SEARCH_QUERY_EMPTY_RESPONSE : "fx.catalog.query.empty_response",
-
-        SEARCH_ANALYZE_SUB: 'resultAnalyze',
-        EDIT_METADATA_SUB : 'clickResultEditMetadata',
-
-        SEARCH_ANALYZE: 'analyze',
-        EDIT_METADATA : "resultEditMetadata"
 
     };
 
