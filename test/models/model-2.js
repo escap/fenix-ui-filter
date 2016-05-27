29d70b8cd1258e311033da14d39483966ae43fbd
Daniele Salvatore <daniele.salvatore@fao.org>
2016-05-27 18:08:49 +0200

    test hideSummary for tree

--- test/models/model-2.js
+++ test/models/model-2.js
@@ -7,7 +7,9 @@ define(function () {
     return {
 
         "Year": {
-            "selector": {"config": {}, "id": "tree", "from": 1960, "to": 2016},
+            "selector": {
+                "config": {}, "id": "tree", "from": 1960, "to": 2016,
+                hideSummary : true},
             "format": {"output": "time", "dimension": "Year"},
             "template": {"title": "Year"}
         }
