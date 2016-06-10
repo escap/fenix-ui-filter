44bfc7d901f45a7108195033fb62900f6b7b8713
Daniele Salvatore <daniele.salvatore@fao.org>
2016-06-10 11:18:35 +0200

    FENIX components options harmonization

--- src/js/filter.js
+++ src/js/filter.js
@@ -187,13 +187,14 @@ define([
 
     /**
      * pub/sub
-     * @return {Object} filter instance
+     * @return {Object} component instance
      */
-    Filter.prototype.on = function (channel, fn) {
+    Filter.prototype.on = function (channel, fn, context) {
+        var _context = context || this;
         if (!this.channels[channel]) {
             this.channels[channel] = [];
         }
-        this.channels[channel].push({context: this, callback: fn});
+        this.channels[channel].push({context: _context, callback: fn});
         return this;
     };
 
