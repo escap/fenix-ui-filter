e92a51a07f3e309e734d0a0643c482d9ad9a3497
Daniele Salvatore <daniele.salvatore@fao.org>
2016-07-26 16:21:41 +0200

    Fix className with template options for selectors, move icon within h5 in selector template

--- test/test.js
+++ test/test.js
@@ -102,7 +102,7 @@ require([
     ], function (log, Test) {
 
         //trace, debug, info, warn, error, silent
-        log.setLevel('trace');
+        log.setLevel('silent');
 
         log.warn("~~~~~ FENIX Filter: test");
         log.info("===== Start testing:...");
