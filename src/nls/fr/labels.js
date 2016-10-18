00a72b12768005d9b81de6dab22021f8bf0a39fa
babic <Barbara.Cintoli@fao.org>
2016-10-18 15:03:34 +0200

    Fr translated file creation

---
+++ src/nls/fr/labels.js
@@ -0,0 +1,15 @@
+define(function () {
+
+    'use strict';
+
+    return {
+
+        enable_filter : "Activé(e)",
+        disable_filter : "Désactiver le filtre",
+        clear_all_btn: "Effacer Tout",
+        select_all_btn: "Sélectionner Tout",
+        search_placeholder: "Filtrer",
+        tooltip_remove_selector: "Supprimer le sélecteur"
+
+    }
+});
