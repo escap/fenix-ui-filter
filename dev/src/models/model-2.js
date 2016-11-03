0d986d4f859b8557ed842b64e93abca111d87a41
mentaltoy <Nicola.Selleri@fao.org>
2016-11-03 11:42:22 +0100

    .19

--- dev/src/models/model-2.js
+++ dev/src/models/model-2.js
@@ -6,28 +6,10 @@ define(function () {
 
     return {
 
-        coverageTime: {
+        tree: {
             selector: {
-                id: "range",
-                format: "DD/MM/YYYY",
-                config: {
-                    type: "double"
-                }
+                id: "tree",
 
-            },
-            template: {
-                "title": "Coverage period",
-                "description": "Information about the time period for which data are available. It requests to report the time window of reference (reporting the starting date and the ending date) even if it presents some lacks.",
-                hideRemoveButton : false,
-                hideSwitch : false,
-
-            }
-        },
-
-        input: {
-            selector: {
-                id: "input",
-                type: "text",
                 source: [{value: "Label", label: "e.g. John Smith"}]
             },
             template: {
@@ -36,46 +18,76 @@ define(function () {
             }
         },
 
-        contact: {
-
-            className: "well",
-
-            semantic: false,
-
-            incremental: true,
-
-            selectors: {
-                name: {
-                    selector: {
-                        id: "input",
-                        type: "text",
-                        source: [{value: "Label", label: "e.g. John Smith"}]
-                    },
-                    template: {
-                        title: "Name"
-                    }
-                },
-                role: {
-                    selector: {
-                        id: "dropdown",
-                        source: [{label: "Project Manager", value: "item_1"}, {label: "Data Owner", value: "item_2"}],
-                        default: "item_1",
-                        config: {
-                            maxItems: 1
-                        }
-                    },
-                    template: {
-                        title: "Role",
-                        description : "The function assumed or part played by a person or thing in a particular situation."
-                    }
-                }
-            },
-
-            template: {
-                hideHeader: false,
-                title: "Contact"
-            }
-        }
+        //coverageTime: {
+        //    selector: {
+        //        id: "range",
+        //        format: "DD/MM/YYYY",
+        //        config: {
+        //            type: "double"
+        //        }
+        //
+        //    },
+        //    template: {
+        //        "title": "Coverage period",
+        //        "description": "Information about the time period for which data are available. It requests to report the time window of reference (reporting the starting date and the ending date) even if it presents some lacks.",
+        //        hideRemoveButton : false,
+        //        hideSwitch : false,
+        //
+        //    }
+        //},
+        //
+        //input: {
+        //    selector: {
+        //        id: "input",
+        //        type: "text",
+        //        source: [{value: "Label", label: "e.g. John Smith"}]
+        //    },
+        //    template: {
+        //        title: "Name",
+        //        description : "My custom description"
+        //    }
+        //},
+        //
+        //contact: {
+        //
+        //    className: "well",
+        //
+        //    semantic: false,
+        //
+        //    incremental: true,
+        //
+        //    selectors: {
+        //        name: {
+        //            selector: {
+        //                id: "input",
+        //                type: "text",
+        //                source: [{value: "Label", label: "e.g. John Smith"}]
+        //            },
+        //            template: {
+        //                title: "Name"
+        //            }
+        //        },
+        //        role: {
+        //            selector: {
+        //                id: "dropdown",
+        //                source: [{label: "Project Manager", value: "item_1"}, {label: "Data Owner", value: "item_2"}],
+        //                default: "item_1",
+        //                config: {
+        //                    maxItems: 1
+        //                }
+        //            },
+        //            template: {
+        //                title: "Role",
+        //                description : "The function assumed or part played by a person or thing in a particular situation."
+        //            }
+        //        }
+        //    },
+        //
+        //    template: {
+        //        hideHeader: false,
+        //        title: "Contact"
+        //    }
+        //}
     }
 
 });
